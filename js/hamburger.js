b525c4049ce21f89faa243c31eff2c2f2f3dd763
MichelleDuque <michelled_1994@hotmail.com>
2024-03-28 00:07:38 -0400

    fixed mobile css

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+// Hamburger Menu
+
+let hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector("nav");
+        
+hamburger.onclick = function() {
+        nav.classList.toggle("active");
+    }
