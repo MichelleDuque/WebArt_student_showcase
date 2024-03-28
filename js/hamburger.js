2d5a029d41f70041761ee83956a3f677b246f32c
MichelleDuque <michelled_1994@hotmail.com>
2024-03-28 02:27:40 -0400

    fixed carousel

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,32 @@
-// Hamburger Menu
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".list-hamburger");
+const check = document.querySelector("#check");
+const hamburgerButtons = document.querySelectorAll(".burger-button");
 
-let hamburger = document.querySelector(".hamburger");
-let nav = document.querySelector("nav");
-        
-hamburger.onclick = function() {
-        nav.classList.toggle("active");
+// Función para abrir el menú
+function openMenu() {
+    nav.classList.add("active");
+}
+
+// Función para cerrar el menú
+function closeMenu() {
+    nav.classList.remove("active");
+    check.checked = false;
+}
+
+//Even listeners
+
+hamburger.addEventListener("click", function() {
+    if (nav.classList.contains("active")) {
+        closeMenu(); 
+    } else {
+        openMenu(); 
     }
+});
+
+hamburgerButtons.forEach(button => {
+    button.addEventListener("click", closeMenu);
+});
+
+
+hamburger.addEventListener("click", openMenu)
