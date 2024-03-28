efb3099bc07a0cc2fe4b16e3d01ee1dc14cbda34
MichelleDuque <michelled_1994@hotmail.com>
2024-03-28 02:34:58 -0400

    deleted unnecessary comments

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,12 +3,10 @@ const nav = document.querySelector(".list-hamburger");
 const check = document.querySelector("#check");
 const hamburgerButtons = document.querySelectorAll(".burger-button");
 
-// Función para abrir el menú
 function openMenu() {
     nav.classList.add("active");
 }
 
-// Función para cerrar el menú
 function closeMenu() {
     nav.classList.remove("active");
     check.checked = false;
